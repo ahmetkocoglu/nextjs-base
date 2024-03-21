75990d6b35aafba6b701f21b2f4c1694713eb7f7
Ahmet Koçoğlu <ahmet.kocoglu@autanate.com>
2024-03-21 15:12:28 +0300

    auth

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignInComponent from "@/components/auth/sign-in";
+import {CustomPage} from "@/layouts/Types";
 
-const SignIn = () => {
+const SignIn: CustomPage = () => {
     return (
         <SignInComponent />
     );
 };
 
+SignIn.Layout = "Auth"
 export default SignIn;
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import SignUpComponent from "@/components/auth/sign-up";
+import {CustomPage} from "@/layouts/Types";
+import SignIn from "@/pages/auth/sign-in";
 
-const SignUp = () => {
+const SignUp: CustomPage = () => {
     return (
         <SignUpComponent />
     );
 };
 
+SignIn.Layout = "Auth"
 export default SignUp;
